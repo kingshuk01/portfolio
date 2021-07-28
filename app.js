09e50f4d14b359214df37b2b53da2d0ebba25021
Kingshuk Mukherjee <kingshukmukherjee2001@gmail.com>
2021-07-29 00:39:03 +0530

    animation

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+
+
+  var tl = gsap.timeline({defaults : {duration:0.6}})
+
+  tl.from('.about-text', {opacity : 0,  y : -50, stagger :0.6})
+  .from('.about-pic',{opacity : 0, y : 50}, "-=0.4")
+  
+
+  var tl2 = gsap.timeline({
+    scrollTrigger:{
+      trigger : '.tech-stack'
+    }
+  })
+
+  tl2.from('.upper img',{x : -2000, stagger :0.15}, "-=0.4")
+  .from('.lower img',{x : -2000, stagger :0.15}, "-=0.4")
+
 function myFunction() {
     
     var copyText = document.getElementById("myInput");
